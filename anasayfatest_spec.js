26ada97719a38f349c2e12b81b97d01d2a58b636
tnrvrd <tanriverdi.hilal@gmail.com>
2016-04-29 14:39:00 +0300

    protractor ile sayfa açılması testi

---
+++ anasayfatest_spec.js
@@ -0,0 +1,8 @@
+describe('test dfraud site for home page',function(){
+	it('to test dfraud homepage for open correcty',function(){
+		browser.ignoreSynchronization = true;
+		browser.get('http://127.0.0.1:9696/#/auth/login');
+		
+	});
+	
+});
